5c7a550b8a57f0b6a45dd6cd40c89e8cc3aa4951
Daniel <dmelgar1110>
2025-10-06 13:27:34 -0600

    Comit1

---
+++ AppApi/AppApi/AppApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApiApp.swift
+//  AppApi
+//
+//  Created by Alumno on 06/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AppApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
